c4e487692acbe527e399ce9bd9e8c7adc7cd694b
hrtlkr29 <kevinsandjose29@gmail.com>
2018-06-29 10:04:52 +0700

    -Working on Academic Fragment     +Add Computer Science major     +Add Business Administration major         ++Included of 4 fragment         ++Fragment pager adapter -Add Notification Service -Add Progress Bar     +Signin Progress Bar     +Signup Progress Bar     +Message Progress Bar

--- app/src/main/java/com/example/pc/campusapplication/AddEventActivity.java
+++ app/src/main/java/com/example/pc/campusapplication/AddEventActivity.java
@@ -1,39 +1,61 @@
 package com.example.pc.campusapplication;
 
 import android.app.DatePickerDialog;
+import android.app.ProgressDialog;
 import android.app.TimePickerDialog;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.TimePicker;
+import android.widget.Toast;
 
-import org.w3c.dom.Text;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.storage.StorageReference;
 
+import java.io.File;
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Locale;
 
 public class AddEventActivity extends AppCompatActivity {
-    TextView edtCalendar;
-    TextView edtTime;
+    TextView tvCalendar;
+    TextView tvTime;
+    TextView tvImage;
     EditText edtName;
     Calendar calendar = Calendar.getInstance();
     int currentHour;
     int currentMinute;
     String amPm;
+    App app;
+    ImageView ivPreview;
+    public static final int TAKE_PICTURE_REQUEST_CODE = 1001;
+    public final static int PICK_PHOTO_REQUEST_CODE = 1002;
+    public String photoFileName = "photo.jpg";
+    File photoFile;
+    DatabaseReference db;
+    StorageReference storageRef;
+    ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_event);
 
-        edtCalendar = findViewById(R.id.txtNewEventDate);
-        edtTime = findViewById(R.id.txtNewEventTime);
+        tvCalendar = findViewById(R.id.txtNewEventDate);
+        tvTime = findViewById(R.id.txtNewEventTime);
         edtName = findViewById(R.id.txtNewEventName);
+        tvImage = findViewById(R.id.txtNewEventImage);
         final DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
             @Override
             public void onDateSet(DatePicker datePicker, int year, int monthOfYear, int dayOfMonth) {
@@ -43,7 +65,7 @@ public class AddEventActivity extends AppCompatActivity {
                 updateLabel();
             }
         };
-        edtCalendar.setOnClickListener(new View.OnClickListener() {
+        tvCalendar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 new DatePickerDialog(AddEventActivity.this,date,calendar
@@ -51,7 +73,7 @@ public class AddEventActivity extends AppCompatActivity {
                         calendar.get(Calendar.DAY_OF_MONTH)).show();
             }
         });
-        edtTime.setOnClickListener(new View.OnClickListener() {
+        tvTime.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 currentHour = calendar.get(Calendar.HOUR_OF_DAY);
@@ -64,10 +86,16 @@ public class AddEventActivity extends AppCompatActivity {
                         } else {
                             amPm = "AM";
                         }
-                        edtTime.setText(String.format("%02d:%02d", hourOfDay, minutes) + amPm);
+                        tvTime.setText(String.format("%02d:%02d", hourOfDay, minutes) + amPm);
                     }
                 }, currentHour, currentMinute, false);
                 timePickerDialog.show();
+            }
+        });
+        tvImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openGallery();
             }
         });
     }
@@ -76,8 +104,46 @@ public class AddEventActivity extends AppCompatActivity {
         String myFormat = "dd/MM/yy"; //In which you need put here
         SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
 
-        edtCalendar.setText(sdf.format(calendar.getTime()));
+        tvCalendar.setText(sdf.format(calendar.getTime()));
     }
+
+    public void openGallery() {
+        // Create intent for picking a photo from the gallery
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+
+        // If you call startActivityForResult() using an intent that no app can handle, your app will crash.
+        // So as long as the result is not null, it's safe to use the intent.
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            // Bring up gallery to select a photo
+            startActivityForResult(intent, PICK_PHOTO_REQUEST_CODE);
+        }
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode == TAKE_PICTURE_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                Bitmap takenImage = BitmapFactory.decodeFile(photoFile.getAbsolutePath());
+                ivPreview = findViewById(R.id.ivNewEventImage);
+                ivPreview.setImageBitmap(takenImage);
+            } else {
+                Toast.makeText(this, "No picture taken", Toast.LENGTH_LONG).show();
+            }
+        } else if (requestCode == PICK_PHOTO_REQUEST_CODE) {
+            Uri photoUri = data.getData();
+            // Do something with the photo based on Uri
+            Bitmap selectedImage = null;
+            try {
+                selectedImage = MediaStore.Images.Media.getBitmap(this.getContentResolver(), photoUri);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            // Load the selected image into a preview
+            ivPreview = (ImageView) findViewById(R.id.ivNewEventImage);
+            ivPreview.setImageBitmap(selectedImage);
+        }
+    }
+
+
     //    private void writeUserData(String uid, String email) {
 //        String firstName = txtFirst.getText().toString().trim();
 //        String lastName = txtLast.getText().toString().trim();
@@ -88,4 +154,12 @@ public class AddEventActivity extends AppCompatActivity {
 //        // go to main activity
 //    }
 
+    private void addEventToDB(String uid){
+        String eventName = edtName.getText().toString().trim();
+        String eventDate = tvCalendar.getText().toString().trim();
+        String eventTime = tvTime.getText().toString().trim();
+        String token = app.loadPref(App.FCM_TOKEN);
+        int eventThumbnail = ivPreview.
+        Event event = new Event(eventName,eventDate,"",eventTime,)
+    }
 }
